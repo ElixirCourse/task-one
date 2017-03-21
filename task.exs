3fd929d1a10c69d5c3f1c55c76b4bb573f91603b
Reductions <slavi.boyanov@gmail.com>
2017-03-21 15:54:03 +0200

    Trying something

--- task.exs
+++ task.exs
@@ -1,8 +1,8 @@
 if_else =
 fn
-  (nil, _, alt) -> alt
-  (false, _, alt) -> alt
-  (_, cons, _) -> cons
+  nil, _, alt -> alt
+  false, _, alt -> alt
+  _, cons, _ -> cons
 end
 
 if_else_lazy = &(if_else.(&1, &2, &3).())
